1c2907ad7a51ef92ae5cb83d720355a3a89659fc
irmlin <irmantas.l75@gmail.com>
2022-03-07 21:39:24 +0200

    adding project files

---
+++ frontend/src/Pages/App.js
@@ -0,0 +1,12 @@
+import '../Styles/App.css';
+import Dashboard from '../Components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
