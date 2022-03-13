39d57b1e12403a9c43309031496c173848e041b4
irmlin1 <irmantas.linkis@ktu.edu>
2022-03-13 14:46:21 +0200

    Added a registration page. It can be accessed by going to /register. Implemened only frontend part (accordingly to the task).

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -1,38 +1,8 @@
-import React, { useState } from "react";
-import { Button, Box, TextField } from "@mui/material";
+import React from "react";
 
 export default function Dashboard (){
-    
-    const [ textValue, setTextValue ] = useState("");
-    const [ clickCount, setClickCount ] = useState(0);
-
-    const onInputChange = (event) => {
-        setTextValue(event.target.value);
-    }
-
-    const onButtonClick = (event) => {
-        setClickCount(clickCount + 1);
-    }
 
     return (
-        <Box sx={{mt:10}}>
-            <Button
-                variant="contained"
-                onClick={onButtonClick}
-                sx={{mr:20}}
-            >
-                Click me
-            </Button>
-
-            <TextField
-                id="outlined-name"
-                label="Enter something"
-                value={textValue}
-                onChange={onInputChange}
-            />
-
-            <div>Your entered: {textValue}</div>
-            <div>Clicked {clickCount} times</div>
-        </Box>
+        <h1>this is our homepage</h1>
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './Pages/App';
+import Register from './Pages/Register'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App/>} />
+                <Route path="/register" element={<Register/>} />
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
